f35a9338b5985177c18668b2e0f8f0f461d7d848
hemali15860 <hemaliskasad.bscibm2024@iar.ac.in>
2025-02-06 15:41:47 +0530

    new file

---
+++ hemali.py
@@ -0,0 +1,19 @@
+# Function to check if a number is even
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+# Example usage
+number = int(input("Enter a number: "))
+if is_even(number):
+    print(f"{number} is even.")
+else:
+    print(f"{number} is not even.")
+
+
+out put :
+Enter a number: 4
+4 is even.
+s
